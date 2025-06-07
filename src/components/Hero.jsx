023794fa3fc0f273212749d2ede0e6af8f53b406
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-08 00:17:31 +0600

    updated code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="hero min-h-[60vh] bg-gradient-to-r from-blue-300 via-violet-200 to-pink-200">
-            <div className="hero-content text-center">
-                <div className="max-w-2xl">
-                    <h1 className="mb-5 text-5xl font-bold text-gray-800">Welcome to GadgetHeaven ✨</h1>
-                    <p className="mb-5 text-lg text-gray-700">
-                        Your go-to destination for the latest and greatest gadgets.
-                    </p>
-                    <button onClick={() => navigate('/dashboard')} className="btn btn-primary">
+        <section className="bg-gradient-to-br from-[#9f70fd] to-[#a167ff] py-12">
+            <div className="max-w-6xl mx-auto text-center px-4">
+                <h1 className="text-4xl sm:text-5xl font-extrabold text-white mb-4">
+                    Upgrade Your Tech Accessorize with <br /> Gadget Heaven Accessories
+                </h1>
+                <p className="text-gray-100 mb-6 max-w-xl mx-auto">
+                    Explore the latest gadgets that will take your experience to the next level.
+                    From smart devices to the coolest accessories, we have it all!
+                </p>
+
+                <div className='text-center'>
+                    <button
+                        onClick={() => navigate('/dashboard')}
+                        className="bg-white text-purple-600 font-semibold px-6 py-2 rounded-full shadow hover:bg-purple-100 transition"            >
                         Shop Now
                     </button>
                 </div>
+                {/* Image Card */}
+                <div className="mt-10 p-1 bg-white rounded-3xl shadow-lg inline-block">
+                    <img
+                        src="/public/assets/banner.jpg" // 👈 Use your actual image path
+                        alt="Gadget Display"
+                        className="rounded-2xl w-full max-w-5xl object-cover"
+                    />
+                </div>
             </div>
-        </div>
+
+        </section>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-100 shadow-sm mb-10">
             {/* Left: Logo */}
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">GadgetsHeaven</a>
